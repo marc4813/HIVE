a5372f1b98469951733abbabc87e3121f552fece
marc4813 <mas1660@knights.ucf.edu>
2023-09-04 14:49:03 -0400

    Add script for installing dependencies

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env/bash
+
+# Vanilla desktop installation of ROS
+sudo apt-get install ros-noetic-desktop-full
+
+# HIVE-specific packages
+sudo apt-get install ros-noetic-move-base
+sudo apt-get install ros-noetic-hector-mapping
+sudo apt-get install ros-noetic-explore-lite
+sudo apt-get install ros-noetic-rosbridge-server
+
