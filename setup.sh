3ad7288ac9df848ddd9bf6c1a7d5faf6a9bcd472
marc4813 <mas1660@knights.ucf.edu>
2023-09-04 14:53:24 -0400

    update dependency script with sources

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env/bash
 
+# Setup sources 
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+
+
 # Vanilla desktop installation of ROS
 sudo apt-get install ros-noetic-desktop-full
 
