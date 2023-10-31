7cc19c1191cf1cc00ef2eb93d32697c19a03118c
KonstantinClonce <konstantin.clonce@gmail.com>
2023-10-30 20:28:36 -0400

    Removed action parameter from the HIghLevelSM.py and from Decider.srv

--- hive_states/src/HighLevelSM.py
+++ hive_states/src/HighLevelSM.py
@@ -2,7 +2,7 @@ import rospy
 from std_msgs.msg import Int32
 from hive_states.srv import Decider
 
-def deciderCallback(id, type, action):
+def deciderCallback(id, type):
     namespace = f'/agent{msg.id}'
     cmdTopic = namespace + '/command'
     actTopic = namespace + '/act'
@@ -10,7 +10,6 @@ def deciderCallback(id, type, action):
     actPublisher = rospy.Publisher(actTopic, Int32, queue_size = 10)
 
     cmdPublisher.publish(Int32(type))
-    actPublisher.publish(Int32(action))
 
 server = rospy.Service('decider', Decider, deciderCallback)
 server.spin()
